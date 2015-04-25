9e9f3a36bace766f38e2739eb445516f58a160f3
Stefan Oestreicher <oestef@gmail.com>
2015-04-25 13:45:27 +0200

    change graph representation to include explicit handler nodes

--- src/DanceMachine.php
+++ src/DanceMachine.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class DanceMachine {
     private $serializers;
-    private $decisions;
+    private $graph;
 
     function __construct() {
         $this->serializers = [
@@ -22,70 +22,97 @@ class DanceMachine {
             'application/php' => 'serialize'
         ];
 
-        $this->decisions = [
-            'post-redirect?' => [[303, ''], 'new?'],
-            'moved-temporarily?' => [[307, ''], 'post-to-gone?'],
-            'valid-entity-length?' => ['is-options?', [413, 'Request entity too large.']],
-            'media-type-available?' => ['accept-language-exists?', [406, 'No acceptable resource available.']],
-            'known-method?' => ['uri-too-long?', [501, 'Unknown method.']],
-            'modified-since?' => ['method-delete?', [304, '']],
-            'etag-matches-for-if-match?' => ['if-unmodified-since-exists?', [412, 'Precondition failed.']],
+        $this->graph = [
+            'handle-ok' => 200,
+            'post-redirect?' => ['handle-see-other', 'new?'],
+            'moved-temporarily?' => ['handle-moved-temporarily', 'post-to-gone?'],
+            'valid-entity-length?' => ['is-options?', 'handle-request-entity-too-large'],
+            'media-type-available?' => ['accept-language-exists?', 'handle-not-acceptable'],
+            'known-method?' => ['uri-too-long?', 'handle-unknown-method'],
+            'modified-since?' => ['method-delete?', 'handle-not-modified'],
+            'etag-matches-for-if-match?' => ['if-unmodified-since-exists?', 'handle-precondition-failed'],
+            'handle-not-found' => 404,
             'if-match-star?' => ['if-unmodified-since-exists?', 'etag-matches-for-if-match?'],
             'post-to-existing?' => ['post!', 'put-to-existing?'],
             'if-modified-since-exists?' => ['if-modified-since-valid-date?', 'method-delete?'],
+            'handle-uri-too-long' => 414,
             'method-put?' => ['put-to-different-url?', 'existed?'],
-            'can-put-to-missing?' => ['conflict?', [501, 'Not implemented.']],
-            'can-post-to-gone?' => ['post!', [410, 'Resource is gone.']],
-            'is-options?' => [[200, ''], 'accept-exists?'],
-            'post-to-missing?' => ['can-post-to-missing?', [404, 'Resource not found.']],
-            'put-to-different-url?' => [[301, ''], 'can-put-to-missing?'],
+            'handle-request-entity-too-large' => 413,
+            'can-put-to-missing?' => ['conflict?', 'handle-not-implemented'],
+            'can-post-to-gone?' => ['post!', 'handle-gone'],
+            'is-options?' => ['handle-options', 'accept-exists?'],
+            'post-to-missing?' => ['can-post-to-missing?', 'handle-not-found'],
+            'handle-gone' => 410,
+            'put-to-different-url?' => ['handle-moved-permanently', 'can-put-to-missing?'],
             'put!' => 'new?',
+            'handle-accepted' => 202,
             'if-match-exists?' => ['if-match-star?', 'if-unmodified-since-exists?'],
-            'language-available?' => ['accept-charset-exists?', [406, 'No acceptable resource available.']],
-            'if-match-star-exists-for-missing?' => [[412, 'Precondition failed.'], 'method-put?'],
-            'processable?' => ['exists?', [422, 'Unprocessable entity.']],
-            'valid-content-header?' => ['known-content-type?', [501, 'Not implemented.']],
+            'language-available?' => ['accept-charset-exists?', 'handle-not-acceptable'],
+            'if-match-star-exists-for-missing?' => ['handle-precondition-failed', 'method-put?'],
+            'processable?' => ['exists?', 'handle-unprocessable-entity'],
+            'valid-content-header?' => ['known-content-type?', 'handle-not-implemented'],
             'if-none-match-star?' => ['if-none-match?', 'etag-matches-for-if-none?'],
-            'conflict?' => [[409, 'Conflict.'], 'put!'],
+            'conflict?' => ['handle-conflict', 'put!'],
+            'handle-precondition-failed' => 412,
             'put-to-existing?' => ['conflict?', 'multiple-representations?'],
-            'allowed?' => ['valid-content-header?', [403, 'Forbidden.']],
+            'allowed?' => ['valid-content-header?', 'handle-forbidden'],
             'existed?' => ['moved-permanently?', 'post-to-missing?'],
-            'service-available?' => ['known-method?', [503, 'Service not available.']],
-            'unmodified-since?' => [[412, 'Precondition failed.'], 'if-none-match-exists?'],
-            'delete-enacted?' => ['respond-with-entity?', [202, 'Accepted']],
+            'handle-created' => 201,
+            'service-available?' => ['known-method?', 'handle-service-not-available'],
+            'handle-forbidden' => 403,
+            'unmodified-since?' => ['handle-precondition-failed', 'if-none-match-exists?'],
+            'handle-options' => 200,
+            'delete-enacted?' => ['respond-with-entity?', 'handle-accepted'],
             'accept-language-exists?' => ['language-available?', 'accept-charset-exists?'],
+            'handle-unauthorized' => 401,
+            'handle-unprocessable-entity' => 422,
             'if-none-match-exists?' => ['if-none-match-star?', 'if-modified-since-exists?'],
-            'charset-available?' => ['accept-encoding-exists?', [406, 'No acceptable resource available.']],
+            'handle-not-acceptable' => 406,
+            'charset-available?' => ['accept-encoding-exists?', 'handle-not-acceptable'],
+            'handle-unsupported-media-type' => 415,
+            'handle-not-implemented' => 501,
             'method-patch?' => ['patch!', 'post-to-existing?'],
             'accept-encoding-exists?' => ['encoding-available?', 'processable?'],
+            'handle-unknown-method' => 501,
+            'handle-multiple-representations' => 300,
             'exists?' => ['if-match-exists?', 'if-match-star-exists-for-missing?'],
+            'handle-moved-temporarily' => 307,
             'method-delete?' => ['delete!', 'method-patch?'],
-            'can-post-to-missing?' => ['post!', [404, 'Resource not found.']],
-            'known-content-type?' => ['valid-entity-length?', [415, 'Unsupported media type.']],
-            'moved-permanently?' => [[301, ''], 'moved-temporarily?'],
+            'handle-not-modified' => 304,
+            'can-post-to-missing?' => ['post!', 'handle-not-found'],
+            'handle-see-other' => 303,
+            'handle-moved-permanently' => 301,
+            'known-content-type?' => ['valid-entity-length?', 'handle-unsupported-media-type'],
+            'moved-permanently?' => ['handle-moved-permanently', 'moved-temporarily?'],
             'if-modified-since-valid-date?' => ['modified-since?', 'method-delete?'],
-            'malformed?' => [[400, 'Bad request.'], 'authorized?'],
+            'malformed?' => ['handle-malformed', 'authorized?'],
             'patch!' => 'respond-with-entity?',
             'if-unmodified-since-valid-date?' => ['unmodified-since?', 'if-none-match-exists?'],
-            'multiple-representations?' => [[300, ''], [200, 'OK']],
+            'handle-no-content' => 204,
+            'multiple-representations?' => ['handle-multiple-representations', 'handle-ok'],
             'delete!' => 'delete-enacted?',
             'post!' => 'post-redirect?',
+            'handle-malformed' => 400,
             'etag-matches-for-if-none?' => ['if-none-match?', 'if-modified-since-exists?'],
-            'respond-with-entity?' => ['multiple-representations?', [204, '']],
-            'method-allowed?' => ['malformed?', [405, 'Method not allowed.']],
-            'uri-too-long?' => [[414, 'Request URI too long.'], 'method-allowed?'],
+            'handle-exception' => 500,
+            'respond-with-entity?' => ['multiple-representations?', 'handle-no-content'],
+            'handle-conflict' => 409,
+            'method-allowed?' => ['malformed?', 'handle-method-not-allowed'],
+            'uri-too-long?' => ['handle-uri-too-long', 'method-allowed?'],
             'if-unmodified-since-exists?' => ['if-unmodified-since-valid-date?', 'if-none-match-exists?'],
-            'post-to-gone?' => ['can-post-to-gone?', [410, 'Resource is gone.']],
+            'post-to-gone?' => ['can-post-to-gone?', 'handle-gone'],
+            'handle-method-not-allowed' => 405,
             'accept-charset-exists?' => ['charset-available?', 'accept-encoding-exists?'],
-            'encoding-available?' => ['processable?', [406, 'No acceptable resource available.']],
-            'authorized?' => ['allowed?', [401, 'Not authorized.']],
+            'encoding-available?' => ['processable?', 'handle-not-acceptable'],
+            'authorized?' => ['allowed?', 'handle-unauthorized'],
             'accept-exists?' => ['media-type-available?', 'accept-language-exists?'],
-            'if-none-match?' => [[304, ''], [412, 'Precondition failed.']],
-            'new?' => [[201, ''], 'respond-with-entity?'],
+            'if-none-match?' => ['handle-not-modified', 'handle-precondition-failed'],
+            'new?' => ['handle-created', 'respond-with-entity?'],
+            'handle-service-not-available' => 503,
         ];
     }
 
-    private function isHandler($node) {
+    private function isAction($node) {
         return is_string($node) && substr($node, -1) == '!';
     }
 
@@ -93,28 +120,34 @@ class DanceMachine {
         return is_string($node) && substr($node, -1) == '?';
     }
 
-    private function isLeafNode($node) {
-        return is_array($node);
+    private function isHandler($node) {
+        return is_string($node) && substr($node, 0, 7) == 'handle-';
     }
 
     private function dispatch(Dance $resource, Song $context, $init = 'service-available?') {
         $node = $init;
-        while (!$this->isLeafNode($node)) {
+        while (!$this->isHandler($node)) {
             if ($this->isDecision($node)) {
-                list($pass, $fail) = $this->decisions[$node];
+                list($pass, $fail) = $this->graph[$node];
                 $result = $resource($node, $context);
                 $node = $result ? $pass : $fail;
-            } else if ($this->isHandler($node)) {
-                $result = $resource($node, $context);
-                if ($result !== null) {
-                    $context->body = strval($result); //TODO: serialize based on media-type
-                }
-                $node = $this->decisions[$node];
+            } else if ($this->isAction($node)) {
+                $resource($node, $context);
+                $node = $this->graph[$node];
             } else {
                 throw new \Exception();
             }
         }
-        return $node;
+        return [$node, $this->graph[$node]];
+    }
+
+    private function runHandler($name, $status, Song $context) {
+        if (isset($context[$name])) {
+            //TODO run handler
+            throw new \Exception('todo');
+        } else {
+            return Response::create('', $status);
+        }
     }
 
     /**
@@ -124,8 +157,8 @@ class DanceMachine {
      */
     function perform(Dance $resource, Request $request = null) {
         $context = new Song($request ?: Request::createFromGlobals(), $resource->conf);
-        list($status, $message) = $this->dispatch($resource, $context);
-        return Response::create($context->body ?: $message, $status);
+        list($handler, $status) = $this->dispatch($resource, $context);
+        return $this->runHandler($handler, $status, $context);
     }
 
     function serialize($value, $mediaType) {
